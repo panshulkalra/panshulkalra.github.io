7f4c222b471abde26bb2bc72b0e6d922a323ce22
Panshul Kalra <panshulkalra@gmail.com>
2026-06-15 13:13:14 +0530

    removed course button

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -73,17 +73,9 @@ const Hero = () => {
        
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
+ 
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
