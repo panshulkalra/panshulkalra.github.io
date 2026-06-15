130c9a8f22a2f8649662c7d2c6251713e604b93a
Panshul Kalra <panshulkalra@gmail.com>
2026-06-15 18:43:29 +0530

    blog

---
+++ src/components/sections/blog.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+import { Link } from 'gatsby';
+
+const StyledBlogSection = styled.section`
+  max-width: 600px;
+  margin: 0 auto 100px;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    margin: 0 auto 50px;
+  }
+
+  .overline {
+    display: block;
+    margin-bottom: 20px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: var(--fz-md);
+    font-weight: 400;
+  }
+
+  .title {
+    font-size: clamp(40px, 5vw, 60px);
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Blog = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  return (
+    <StyledBlogSection id="blog" ref={revealContainer}>
+      <span className="overline">What's Cooking?</span>
+      <h2 className="title">My Blogs</h2>
+
+      <p>
+        I write deeply technical post-mortems and analytical essays breaking down modern systems infrastructure, advanced algorithms, and market scaling strategies.
+      </p>
+
+      <Link className="email-link" to="/blog">
+        Read my blog &rarr;
+      </Link>
+    </StyledBlogSection>
+  );
+};
+
+export default Blog;
