42c08b4abda80d83b48862efb1c287e68598cf67
panshulkalra <panshulkalra@users.noreply.github.com>
2026-06-16 08:37:26 +0000

    deploy: 8359bca48c659396247995f3c05e629b3c957b6d

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-54d8ac5352df90fb0d61.js"
+    "url": "webpack-runtime-7e811cf6add4f6ee38d0.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1851b6766b449d545f641a24f8bc38ac"
+    "revision": "ead441a38404a8416e51f510b3a02e98"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
